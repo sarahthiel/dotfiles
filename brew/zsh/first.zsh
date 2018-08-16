185262c7f0eca6eb222d22a5d0c80bfae3064055
Sebastian Thiel <s.thiel@sae.edu>
2018-08-16 11:21:53 +0200

    install brew fonts into users font folder

--- brew/zsh/first.zsh
+++ brew/zsh/first.zsh
@@ -2,4 +2,4 @@ export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_INSECURE_REDIRECT=1
 export HOMEBREW_CASK_OPTS=--require-sha
 export HOMEBREW_UPGRADE_CLEANUP=1
-export HOMEBREW_CASK_OPTS="--appdir=~/Applications --fontdir=/Library/Fonts"
+export HOMEBREW_CASK_OPTS="--appdir=~/Applications --fontdir=~/Library/Fonts"
